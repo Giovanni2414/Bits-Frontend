8a0506a0d42efebef31a07a965a45f3ecad73fbf
Juan Camilo <61602045+juanmiloz@users.noreply.github.com>
2023-03-18 15:01:50 -0500

    add Hexagonal Architecture

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import SignIn from './Components/SignIn'
+import SignIn from './pages/Components/SignIn'
 
 function App() {
 

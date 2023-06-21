086be785a0effe13bc826b475d2a89109285cb87
Giovanni Mosquera <Giovanni2414g@gmail.com>
2023-06-21 13:10:12 -0500

    Cambios

--- src/pages/Services/axiosService.js
+++ src/pages/Services/axiosService.js
@@ -19,6 +19,7 @@ export class CRUDService {
             .then(res => res)
             .catch(function (error) {
                 console.log(url);
+                console.log(process.env.REACT_APP_BACKEND_URL);
                 if (error.response) {
                     // Request made and server responded
                     alert("ERROR " + error.response.data.code + "\n" + error.response.data.message);
